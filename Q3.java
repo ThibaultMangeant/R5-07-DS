1e632afdfa79fd3f4093a66dbb0e1ac60149318f
Mangeant Thibault <mangeant.thibault@gmail.com>
2025-10-08 15:58:22 +0200

    Maj Q3.java et Creation Q4.java

--- Q3.java
+++ Q3.java
@@ -2,14 +2,14 @@ public class Q3
 {
 	public static void main(String[] args)
 	{
-		if (args.length == 0)
+		if (args.length != 1)
 		{
 			System.out.println("Usage : java Q3 <nombre>");
 			return;
 		}
     
 		int n = Integer.parseInt(args[0]);
-		for (int i = 0; i < n; i++) System.out.print("*");
+		for (int i = 1; i <= n; i++) System.out.print(i + " ");
 		System.out.println();
 	}
 }
